71d2b37aed529d8a0776838fcfbf14cb3da1d410
Krishan Kant Sharma <kkssharma071@gmail.com>
2026-02-20 15:03:04 +0530

    Refactor script.js by removing unused features
    Removed boot screen functionality, start menu toggle, context menu handling, and icon drag events.

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let z=1;
 
+/* WARNING CLOSE */
+function startOS(){
+document.getElementById("winWarning").style.display="none";
+}
+
 /* CLOCK */
 setInterval(()=>{
 document.getElementById("clock")
 .innerText=new Date().toLocaleTimeString();
 },1000);
 
-/* BOOT */
-window.onload=()=>{
-setTimeout(()=>{
-document.getElementById("bootScreen").style.display="none";
-},2500);
-};
-
 /* WINDOWS */
+
 function openWindow(id){
-const w=document.getElementById(id);
+let w=document.getElementById(id);
 w.classList.remove("hidden");
 w.style.zIndex=++z;
 }
 
 function closeWindow(id){
-document.getElementById(id).classList.add("hidden");
+document.getElementById(id)
+.classList.add("hidden");
 }
 
 function minimizeWindow(id){
@@ -29,54 +29,10 @@ closeWindow(id);
 }
 
 /* THEME */
+
 function changeTheme(t){
 document.documentElement.style
 .setProperty("--bg-color",
 t==="dark"?"#222":
 t==="pink"?"#ff71ce":"#008080");
 }
-
-/* START MENU */
-document.querySelector(".start-btn")
-.onclick=()=>{
-document.getElementById("startMenu")
-.classList.toggle("hidden");
-};
-
-/* RIGHT CLICK */
-document.addEventListener("contextmenu",e=>{
-e.preventDefault();
-const m=document.getElementById("contextMenu");
-m.style.top=e.pageY+"px";
-m.style.left=e.pageX+"px";
-m.classList.remove("hidden");
-});
-
-document.addEventListener("click",()=>{
-contextMenu.classList.add("hidden");
-});
-
-/* ICON DRAG */
-document.querySelectorAll(".icon")
-.forEach(icon=>{
-icon.onmousedown=function(e){
-let shiftX=e.clientX-icon.getBoundingClientRect().left;
-let shiftY=e.clientY-icon.getBoundingClientRect().top;
-
-function moveAt(pageX,pageY){
-icon.style.left=pageX-shiftX+"px";
-icon.style.top=pageY-shiftY+"px";
-}
-
-function onMouseMove(e){
-moveAt(e.pageX,e.pageY);
-}
-
-document.addEventListener("mousemove",onMouseMove);
-
-icon.onmouseup=function(){
-document.removeEventListener("mousemove",onMouseMove);
-icon.onmouseup=null;
-};
-};
-});
